34b641da065b1a5b17254372fff55fa2989ba9ee
Daland Montgomery <davidmontgomery@gmail.com>
2015-09-20 18:44:10 +0800

    update

--- recipes/monit-sentinal.rb
+++ recipes/monit-sentinal.rb
@@ -2,10 +2,9 @@
 
 
 service "monit"
-
-template "/etc/monit/conf.d/redis.conf" do
-  path "/etc/monit/conf.d/redis.conf"
-  source "monit.redis.cluster.conf.erb"
+template "/etc/monit/conf.d/sentinal.conf" do
+  path "/etc/monit/conf.d/sentinal.conf"
+  source "monit.sentinal.conf.erb"
   owner "root"
   group "root"
   mode "0755"
